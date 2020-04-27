9846b999540d57ae3320315a5e2a78683f8dca59
ABDU104 <elvin.abduyev@bertelsmann.de>
2020-04-27 10:52:29 +0200

    all changes done

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
   $('#example').DataTable();
 });
+
+$(document).ready(function() {
+    $.each($('#navbar').find('li'), function() {
+        $(this).toggleClass('active',
+            window.location.pathname.indexOf($(this).find('a').attr('href')) > -1);
+    });
+});
